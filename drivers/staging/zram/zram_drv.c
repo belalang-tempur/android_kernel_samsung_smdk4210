2df548948da2974047bc60ad89fb9068896e9c09
Readone !! ErcOne <android-armv7a-belalang-tempur@users.noreply.github.com>
2015-07-01 05:11:07 +0700

    zram default 2 devices

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -693,6 +693,9 @@ static void destroy_device(struct zram *zram)
 static int __init zram_init(void)
 {
 	int ret, dev_id;
+	
+	//initialize with default of 2 zRam devices
+	num_devices = 2;
 
 	if (num_devices > max_num_devices) {
 		pr_warning("Invalid value for num_devices: %u\n",
